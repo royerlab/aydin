1e6afd0617154d224d0c84b4776488ae6e5feeca
Ahmet Can Solak <asolak14@ku.edu.tr>
2019-06-20 16:27:18 -0700

    GUI Introduction (#12)
    * to initiate this branch
    
    * updating dev branch from master (#2)
    
    * Update README.md
    
    * final changes before hollidays...
    
    * added a placeholder for the cnn version
    
    * Condaenv refactor (#1)
    
    * to initiate this branch
    
    * updated condaenv.yml
    
    * Gitignore idea update (#3)
    
    * Update README.md
    
    * final changes before hollidays...
    
    * added a placeholder for the cnn version
    
    * Condaenv refactor (#1)
    
    * to initiate this branch
    
    * updated condaenv.yml
    
    * git ignore idea update
    
    * 2D tests are separated into separate files (#4)
    
    * Release folder structure (#5)
    
    * release folder structure and readme initiated
    
    * release doc name change
    
    * release doc name change again
    
    * release folder structure initiated with dummy files
    
    * Basic CLI, Web object resource fetching extracting (#6)
    
    * WIP:initial commit on cli
    
    * web object download and extract method implemented with test and it is passing, current examples updated with this data accordingly however most examples are exploiting memory till crash, gitignore file updated accordingly
    
    * proper cli structure implemented with working run demo 2D command
    
    * readme todo update (#7)
    
    * Noise2Self Service and initial service package (#8)
    
    * service package added
    
    * noise2self service and its tests added, passing
    
    * Noise2Truth inital implementation (#10)
    
    * drag and drop and button file path picket component created and tested
    
    * typo fix
    
    * gui introduced with noise2self integration

--- src/pitl/util/resource.py
+++ src/pitl/util/resource.py
@@ -3,6 +3,10 @@ from pathlib import Path
 import os
 import zipfile
 
+from skimage.exposure import rescale_intensity
+from skimage.io import imread
+import numpy as np
+
 
 def download_and_extract_zipresource(url, targetdir='.'):
 
@@ -26,3 +30,7 @@ def download_and_extract_zipresource(url, targetdir='.'):
 
         # Delete zip file
         Path.unlink(Path(relative_path_to_zip))
+
+def read_image_from_path(path):
+    image = imread(path).astype(np.float32)
+    return rescale_intensity(image, in_range='image', out_range=(0, 1))
