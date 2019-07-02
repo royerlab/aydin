9fb78bfe7f53a23a1c5ace2f5313069d5f7bf244
Ahmet Can Solak <asolak14@ku.edu.tr>
2019-07-01 17:49:03 -0700

    cli noise2self integration (#18)

--- src/pitl/util/resource.py
+++ src/pitl/util/resource.py
@@ -31,6 +31,7 @@ def download_and_extract_zipresource(url, targetdir='.'):
         # Delete zip file
         Path.unlink(Path(relative_path_to_zip))
 
+
 def read_image_from_path(path):
     image = imread(path).astype(np.float32)
     return rescale_intensity(image, in_range='image', out_range=(0, 1))
