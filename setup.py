1f92dc75e47b1f69b3b4873d7ad6f1265ad7fb3b
acs-home-ws <asolak14@ku.edu.tr>
2021-11-06 11:17:11 -0700

    initial commit

--- setup.py
+++ setup.py
@@ -29,15 +29,22 @@ setup(
     packages=['aydin'],
     use_scm_version=True,
     setup_requires=['setuptools_scm'],
+    url='https://github.com/royerlab/aydin',
     install_requires=INSTALL_REQUIRES,
-    py_modules=['aydin'],
+    # py_modules=['aydin'],
     entry_points='''
             [console_scripts]
             aydin=aydin.cli.cli:aydin
         ''',
-    download_url='https://github.com/royerlab/aydin/archive/refs/tags/v0.1.6.tar.gz',
+    license='BSD 3-Clause',
+    license_file='LICENSE.txt',
     classifiers=[
         'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.9',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Image Processing',
       ],
 )
