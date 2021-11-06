ba1631ce4b8c757af1d5c547ee0c08a2b8cf1f42
acs-home-ws <asolak14@ku.edu.tr>
2021-11-06 11:55:08 -0700

    to keep changes

--- setup.py
+++ setup.py
@@ -25,26 +25,10 @@ REQUIRES = []
 # os.system("pip install -r " + os.path.join('requirements', 'pyopencl.txt'))
 
 setup(
-    name='aydin',
-    # packages=['aydin'],
-    # use_scm_version=True,
-    version="0.0.6",
-    # setup_requires=['setuptools_scm'],
-    url='https://github.com/royerlab/aydin',
+    use_scm_version={"write_to": "aydin/_version.py"},
+    setup_requires=['setuptools_scm'],
     install_requires=INSTALL_REQUIRES,
-    py_modules=['aydin'],
     entry_points={
         'console_scripts': ['aydin=aydin.cli.cli:cli']
         },
-    license='BSD 3-Clause',
-    license_file='LICENSE.txt',
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'License :: OSI Approved :: BSD License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.9',
-        'Intended Audience :: Science/Research',
-        'Topic :: Scientific/Engineering :: Image Processing',
-      ],
 )
