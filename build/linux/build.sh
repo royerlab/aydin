9bcdd843ff34f36a4bea5290c200e94e65bc2514
Ahmet Can Solak <asolak14@ku.edu.tr>
2021-12-21 11:18:25 -0800

    initial commit, implemented (#68)
    Co-authored-by: acs-ws <asolak@ku.edu.tr>

--- build/linux/build.sh
+++ build/linux/build.sh
@@ -13,4 +13,6 @@ echo "building app..."
 #onefile
 pyinstaller -w -F -y --clean aydin.spec
 
+mkdir -p dist/aydin/numba/experimental/jitclass
+cp /PATH/TO/numba/experimental/jitclass/_box.cpython-39-x86_64-linux-gnu.so dist/aydin/numba/experimental/jitclass/.
 cp run_aydin.sh dist/.
