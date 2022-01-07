ccf4227cfc2500f9472c4e6d52e95f82268a3c52
acs-ws <asolak@ku.edu.tr>
2022-01-07 14:50:49 -0800

    reverting unnecessary change

--- aydin/restoration/denoise/util/test/test_denoise_utils.py
+++ aydin/restoration/denoise/util/test/test_denoise_utils.py
@@ -4,10 +4,7 @@ from aydin.restoration.denoise.util.denoise_utils import (
     get_list_of_denoiser_implementations,
 )
 
-implementations = get_list_of_denoiser_implementations()
-pprint(implementations)
-
 
 def est_denoise_utils():
     implementations = get_list_of_denoiser_implementations()
-    print(implementations)
+    pprint(implementations)
