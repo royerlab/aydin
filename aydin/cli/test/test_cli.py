0bf6eed664e7a499e45c20e2d364d0098c941ae0
AhmetCanSolak <asolak14@ku.edu.tr>
2022-04-08 08:00:16 -0700

    to  keep changes

--- aydin/cli/test/test_cli.py
+++ aydin/cli/test/test_cli.py
@@ -2,22 +2,28 @@ from click.testing import CliRunner
 
 from aydin.cli.cli import cli
 from aydin.io.datasets import examples_single
+from aydin.util.log.log import Log
 
 
 def test_info():
+    Log.override_test_exclusion = True
     image_path = examples_single.generic_lizard.get_path()
-    print(image_path)
 
     runner = CliRunner()
     result = runner.invoke(cli, ['info', image_path])
     print(result.output)
+    print(result.stdout_bytes)
+
     assert result.exit_code == 0
-    assert "batch" in result.output
+    assert "Reading" in result.stdout_bytes
+    print(result.output)
+    assert "Metadata" in result.stdout
 
 
 def test_cite():
     runner = CliRunner()
     result = runner.invoke(cli, ['cite'])
+    print(result.output)
     assert result.exit_code == 0
     assert "10.5281/zenodo.5654826" in result.output
 
