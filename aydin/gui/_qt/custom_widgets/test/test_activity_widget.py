2af16c3917f320844fbde741aa6e92e6828773a3
AhmetCanSolak <asolak14@ku.edu.tr>
2022-04-08 17:51:48 -0700

    improved activity widget test

--- aydin/gui/_qt/custom_widgets/test/test_activity_widget.py
+++ aydin/gui/_qt/custom_widgets/test/test_activity_widget.py
@@ -3,7 +3,7 @@ from qtpy.QtWidgets import QWidget
 from aydin.gui._qt.custom_widgets.activity_widget import ActivityWidget
 
 
-def test_hello(qtbot):
+def test_activity_widget(qtbot):
     dummy_parent = QWidget()
     widget = ActivityWidget(dummy_parent)
     qtbot.addWidget(widget)
@@ -11,6 +11,8 @@ def test_hello(qtbot):
     widget.activity_print("testinghard")
     widget.activity_print("testingharder")
 
-    print(widget.infoTextBox.toPlainText())
-
     assert widget.infoTextBox.toPlainText() == "testinghard" + "testingharder"
+
+    widget.clear_activity()
+
+    assert widget.infoTextBox.toPlainText() == ""
