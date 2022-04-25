09100716556b7f0e1965be939eb02aa843a10bb6
AhmetCanSolak <asolak14@ku.edu.tr>
2022-04-25 08:09:01 -0700

    initial commit, function signature added

---
+++ aydin/analysis/noise_floor_delta.py
@@ -0,0 +1,19 @@
+
+
+def noise_floor_delta_over_z(noisy_array, denoised_array) -> float:
+    """
+    This method calculates the noise floor level difference on spectra
+    of a noisy and a denoised array (does NOT require groundtruth). 
+
+    Parameters
+    ----------
+    denoised_array : numpy.typing.ArrayLike
+    noisy_array : numpy.typing.ArrayLike
+
+    Returns
+    -------
+    float
+        Calculated noise floor difference between the noisy and denoised arrays.
+
+    """
+    pass
