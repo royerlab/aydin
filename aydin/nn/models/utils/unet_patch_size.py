fa56b21d649062d00f9b4160406f97f078c2c7fb
AhmetCanSolak <asolak14@ku.edu.tr>
2022-06-14 14:48:46 -0700

    patch size calculation refactored into its util method

---
+++ aydin/nn/models/utils/unet_patch_size.py
@@ -0,0 +1,41 @@
+from aydin.util.log.log import lprint
+
+
+def get_ideal_patch_size(nb_unet_levels, training_architecture):
+    patch_size = (
+            get_receptive_field_radius(
+                nb_unet_levels,
+                shiftconv='shiftconv' == training_architecture,
+            )
+            * 2
+    )
+
+    patch_size -= patch_size % 2 ** nb_unet_levels
+
+    if patch_size < 2 ** nb_unet_levels:
+        raise ValueError(
+            'Number of layers is too large for given patch size.'
+        )
+
+    lprint(f'Patch size: {patch_size}')
+    return patch_size
+
+
+def get_receptive_field_radius(nb_unet_levels, shiftconv=False):
+    """TODO: add proper docstrings here
+
+    Parameters
+    ----------
+    nb_unet_levels : int
+    shiftconv : bool
+
+    Returns
+    -------
+    int
+
+    """
+    if shiftconv:
+        rf = 7 if nb_unet_levels == 0 else 36 * 2 ** (nb_unet_levels - 1) - 6
+    else:
+        rf = 3 if nb_unet_levels == 0 else 18 * 2 ** (nb_unet_levels - 1) - 4
+    return int(rf // 2)
