2a9949750c112067d5c39ad325915d9c077fa53c
Ahmet Can Solak <asolak14@ku.edu.tr>
2022-07-18 16:55:50 -0700

    Torch unet fstring fixes (#213)
    * initial commit, implemented
    
    * uncommented log statement
    
    Co-authored-by: acs-ws <asolak@ku.edu.tr>

--- aydin/nn/models/torch/torch_unet.py
+++ aydin/nn/models/torch/torch_unet.py
@@ -203,7 +203,7 @@ def n2t_unet_train_loop(
         for i, (input_image, target_image) in enumerate(
             zip([input_images], [target_images])
         ):
-            # print(f"index: {i}, shape:{input_image.shape}")
+            lprint(f"index: {i}, shape:{input_image.shape}")
 
             # Clear gradients w.r.t. parameters
             optimizer.zero_grad()
@@ -247,10 +247,10 @@ def n2t_unet_train_loop(
                 iteration += 1
 
         train_loss_value /= iteration
-        lprint("Training loss value: {train_loss_value}")
+        lprint(f"Training loss value: {train_loss_value}")
 
         val_loss_value /= iteration
-        lprint("Validation loss value: {val_loss_value}")
+        lprint(f"Validation loss value: {val_loss_value}")
 
         writer.add_scalar("Loss/train", train_loss_value, epoch)
         writer.add_scalar("Loss/valid", val_loss_value, epoch)
@@ -283,11 +283,11 @@ def n2t_unet_train_loop(
 
             # No improvement:
             lprint(
-                "No improvement of validation losses, patience = {patience_counter}/{self.patience} "
+                f"No improvement of validation losses, patience = {patience_counter}/{patience} "
             )
             patience_counter += 1
 
-        lprint("## Best val loss: {best_val_loss_value}")
+        lprint(f"## Best val loss: {best_val_loss_value}")
 
         # if epoch % 512 == 0:
         #     print(epoch)
