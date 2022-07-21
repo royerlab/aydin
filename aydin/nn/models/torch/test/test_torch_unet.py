952149856f7da271724a7b3ef74dca6da081376d
acs-ws <asolak@ku.edu.tr>
2022-07-21 10:51:30 -0700

    3d test also parametrized

--- aydin/nn/models/torch/test/test_torch_unet.py
+++ aydin/nn/models/torch/test/test_torch_unet.py
@@ -52,16 +52,14 @@ def test_supervised_2D_n2t():
     # assert result.dtype == input_image.dtype
 
 
-@pytest.mark.parametrize(
-    "nb_unet_levels,spacetime_ndim", [(2, 2), (3, 2), (5, 2), (8, 2)]
-)
-def test_masking_2D(nb_unet_levels, spacetime_ndim):
+@pytest.mark.parametrize("nb_unet_levels", [2, 3, 5, 8])
+def test_masking_2D(nb_unet_levels):
     input_array = torch.zeros((1, 1, 1024, 1024))
     model2d = UNetModel(
         # (64, 64, 1),
         nb_unet_levels=nb_unet_levels,
         supervised=False,
-        spacetime_ndim=spacetime_ndim,
+        spacetime_ndim=2,
     )
     result = model2d(input_array, torch.ones(input_array.shape))
     assert result.shape == input_array.shape
@@ -81,11 +79,12 @@ def test_supervised_3D():
     assert result.dtype == input_array.dtype
 
 
-def test_masking_3D():
+@pytest.mark.parametrize("nb_unet_levels", [2, 3, 5])
+def test_masking_3D(nb_unet_levels):
     input_array = torch.zeros((1, 1, 64, 64, 64))
     model3d = UNetModel(
         # (64, 64, 64, 1),
-        nb_unet_levels=2,
+        nb_unet_levels=nb_unet_levels,
         supervised=False,
         spacetime_ndim=3,
     )
