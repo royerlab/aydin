a4454b87eb85c3e812b15e6b91d105a60cd7edfe
Ahmet Can Solak <asolak14@ku.edu.tr>
2022-07-21 13:15:50 -0700

    Torch original unet (#216)
    * initial commit
    
    * black fixes
    
    * working version
    
    * removing run files
    
    * one test parametrized
    
    * 3d test also parametrized
    
    Co-authored-by: acs-ws <asolak@ku.edu.tr>

--- aydin/nn/models/torch/test/test_torch_unet.py
+++ aydin/nn/models/torch/test/test_torch_unet.py
@@ -1,24 +1,11 @@
 # flake8: noqa
-
-# import math
-# from collections import OrderedDict
-# from itertools import chain
-#
-# import napari
 import numpy
 import pytest
 import torch
 
-# from torch.optim.lr_scheduler import ReduceLROnPlateau
-from torch.utils.data import DataLoader
-
 from aydin.io.datasets import add_noise, camera, normalise
 from aydin.nn.models.torch.torch_unet import UNetModel, n2t_unet_train_loop
-from aydin.nn.models.utils.torch_dataset import TorchDataset
-
-# from aydin.nn.pytorch.it_ptcnn import to_numpy
-# from aydin.nn.pytorch.optimizers.esadam import ESAdam
-# from aydin.util.log.log import lprint
+from aydin.nn.pytorch.it_ptcnn import to_numpy
 
 
 def test_supervised_2D():
@@ -28,7 +15,6 @@ def test_supervised_2D():
         nb_unet_levels=2,
         supervised=True,
         spacetime_ndim=2,
-        residual=True,
     )
     result = model2d(input_array)
     assert result.shape == input_array.shape
@@ -36,7 +22,8 @@ def test_supervised_2D():
 
 
 def test_supervised_2D_n2t():
-    lizard_image = normalise(camera()[:128, :128])
+    visualize = False
+    lizard_image = normalise(camera()[:256, :256])
     lizard_image = numpy.expand_dims(lizard_image, axis=0)
     lizard_image = numpy.expand_dims(lizard_image, axis=0)
 
@@ -45,41 +32,38 @@ def test_supervised_2D_n2t():
     input_image = torch.tensor(input_image)
     lizard_image = torch.tensor(lizard_image)
 
-    # dataset = TorchDataset(input_image, lizard_image, 64, self_supervised=False)
-
-    # data_loader = DataLoader(
-    #     dataset, batch_size=1, shuffle=True, num_workers=0, pin_memory=True
-    # )
-
-    model = UNetModel(
-        nb_unet_levels=2, supervised=True, spacetime_ndim=2, residual=True
-    )
+    model = UNetModel(nb_unet_levels=2, supervised=True, spacetime_ndim=2)
 
     n2t_unet_train_loop(input_image, lizard_image, model)
+
+    denoised = model(input_image)
+
+    if visualize:
+        import napari
+
+        viewer = napari.Viewer()
+        viewer.add_image(to_numpy(lizard_image), name="groundtruth")
+        viewer.add_image(to_numpy(input_image), name="noisy")
+        viewer.add_image(to_numpy(denoised), name="denoised")
+
+        napari.run()
 
     # assert result.shape == input_image.shape
     # assert result.dtype == input_image.dtype
 
 
-def test_masking_2D():
-    input_array = torch.zeros((1, 1, 64, 64))
+@pytest.mark.parametrize("nb_unet_levels", [2, 3, 5, 8])
+def test_masking_2D(nb_unet_levels):
+    input_array = torch.zeros((1, 1, 1024, 1024))
     model2d = UNetModel(
         # (64, 64, 1),
-        nb_unet_levels=2,
+        nb_unet_levels=nb_unet_levels,
         supervised=False,
         spacetime_ndim=2,
     )
     result = model2d(input_array, torch.ones(input_array.shape))
     assert result.shape == input_array.shape
     assert result.dtype == input_array.dtype
-
-
-# def test_jinet_2D():
-#     input_array = torch.zeros((1, 1, 64, 64))
-#     model2d = JINetModel((64, 64, 1), spacetime_ndim=2)
-#     result = model2d.predict([input_array])
-#     assert result.shape == input_array.shape
-#     assert result.dtype == input_array.dtype
 
 
 def test_supervised_3D():
@@ -95,11 +79,12 @@ def test_supervised_3D():
     assert result.dtype == input_array.dtype
 
 
-def test_masking_3D():
+@pytest.mark.parametrize("nb_unet_levels", [2, 3, 5])
+def test_masking_3D(nb_unet_levels):
     input_array = torch.zeros((1, 1, 64, 64, 64))
     model3d = UNetModel(
         # (64, 64, 64, 1),
-        nb_unet_levels=2,
+        nb_unet_levels=nb_unet_levels,
         supervised=False,
         spacetime_ndim=3,
     )
