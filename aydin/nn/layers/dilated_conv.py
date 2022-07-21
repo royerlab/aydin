9d106a17723b8efc51025ed52be2cc87c92f89bf
acs-ws <asolak@ku.edu.tr>
2022-07-21 16:38:56 -0700

    dilated conv implemented

---
+++ aydin/nn/layers/dilated_conv.py
@@ -0,0 +1,49 @@
+from torch import nn
+from torch.nn import ZeroPad2d
+
+
+class DilatedConv(nn.Module):
+    def __init__(
+            self,
+            in_channels,
+            out_channels,
+            spacetime_ndim,
+            padding,
+            kernel_size,
+            dilation,
+            activation="ReLU",
+    ):
+        super(DilatedConv, self).__init__()
+
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.spacetime_ndim = spacetime_ndim
+        self.activation = activation
+
+        self.zero_padding = ZeroPad2d(padding)
+
+        if spacetime_ndim == 2:
+            self.conv = nn.Conv2d(
+                in_channels, out_channels, kernel_size, dilation=dilation,  padding='same'
+            )
+        elif spacetime_ndim == 3:
+            self.conv = nn.Conv3d(
+                in_channels, out_channels, kernel_size, dilation=dilation, padding='same'
+            )
+        else:
+            raise ValueError("spacetime_ndim parameter can only be 2 or 3...")
+
+        self.activation_function = {
+            "ReLU": nn.ReLU(),
+            "swish": nn.SiLU(),
+            "lrel": nn.LeakyReLU(0.1),
+        }[self.activation]
+
+    def forward(self, x):
+        x = self.zero_padding(x)
+
+        x = self.conv(x)
+
+        x = self.activation_function(x)
+
+        return x
