3685028d4ed9787d78dada7e80ff49dd874fd4aa
acs-ws <asolak@ku.edu.tr>
2022-07-25 16:05:57 -0700

    fixed the prediction call in the test casE

--- aydin/nn/models/torch/test/test_torch_jinet.py
+++ aydin/nn/models/torch/test/test_torch_jinet.py
@@ -6,6 +6,6 @@ from aydin.nn.models.torch.torch_jinet import JINetModel
 def test_forward_2D_jinet():
     input_array = torch.zeros((1, 1, 64, 64))
     model2d = JINetModel(spacetime_ndim=2)
-    result = model2d.predict([input_array])
+    result = model2d(input_array)
     assert result.shape == input_array.shape
     assert result.dtype == input_array.dtype
