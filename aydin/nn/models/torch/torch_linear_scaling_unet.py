f6565a964c3da422cb34bd9e06368c2b47febdc5
acs-ws <asolak@ku.edu.tr>
2022-07-27 09:44:58 -0700

    forward method added

--- aydin/nn/models/torch/torch_linear_scaling_unet.py
+++ aydin/nn/models/torch/torch_linear_scaling_unet.py
@@ -1,3 +1,4 @@
+import torch
 from torch import nn
 
 from aydin.nn.layers.pooling_down import PoolingDown
@@ -25,5 +26,34 @@ class LinearScalingUNetModel(nn.Module):
 
         self.unet_bottom_conv_out_channels = self.nb_filters * (self.nb_unet_levels - 1)
 
-    def forward(self, x):
+    def forward(self, x, input_mask):
+        skip_layer = []
+
+        # Encoder
+        for layer_index in range(self.nb_unet_levels):
+            x = self.double_conv_blocks_encoder[layer_index](x)
+            skip_layer.append(x)
+            x = self.pooling_down(x)
+
+        # Bottom
+        x = self.unet_bottom_conv_block(x)
+
+        # Decoder
+        for layer_index in range(self.nb_unet_levels):
+            x = self.upsampling(x)
+            x = torch.cat([x, skip_layer.pop()], dim=1)
+            x = self.double_conv_blocks_decoder[layer_index](x)
+
+        # Final convolution
+        x = self.final_conv(x)
+
+        # Masking for self-supervised training
+        if not self.supervised:
+            if input_mask is not None:
+                x *= input_mask
+            else:
+                raise ValueError(
+                    "input_msk cannot be None for self-supervised training"
+                )
+
         return x
