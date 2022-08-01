365deab719df8fa529836f507ab3d41a26160fe0
acs-ws <asolak@ku.edu.tr>
2022-08-01 09:51:16 -0700

    tests TBD

---
+++ aydin/analysis/test/test_fsc.py
@@ -0,0 +1,17 @@
+import pytest
+
+from aydin.analysis.fsc import shell_sum, fsc
+
+
+# @pytest.mark.parametrize("image", [2, 3, 5, 8])
+def test_shell_sum(image, length):
+    result = shell_sum(image)
+
+    assert len(result) == length
+
+
+def test_fsc(image1, image2):
+    correlations = fsc(image1, image2)
+
+    assert correlations.shape == image1.shape == image2.shape
+
