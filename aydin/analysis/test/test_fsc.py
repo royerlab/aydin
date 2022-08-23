24b41a514426b70100f44417f8df5696e693fd99
acs-ws <asolak@ku.edu.tr>
2022-08-23 10:41:39 -0700

    tests are implemented

--- aydin/analysis/test/test_fsc.py
+++ aydin/analysis/test/test_fsc.py
@@ -1,16 +1,26 @@
-# import pytest
+import pytest
+from numpy.random.mtrand import normal
 
 from aydin.analysis.fsc import shell_sum, fsc
+from aydin.io.datasets import camera, normalise, newyork
 
 
-# @pytest.mark.parametrize("image", [2, 3, 5, 8])
+@pytest.mark.parametrize("image, length", [(camera(), 363), (newyork(), 725)])
 def test_shell_sum(image, length):
     result = shell_sum(image)
 
     assert len(result) == length
 
 
-def test_fsc(image1, image2):
+@pytest.mark.parametrize("clean_image", [camera(), newyork()])
+def test_fsc(clean_image):
+    clean_image = normalise(clean_image)
+    noise1 = normal(size=clean_image.size).reshape(*clean_image.shape)
+    noise2 = normal(size=clean_image.size).reshape(*clean_image.shape)
+
+    image1 = clean_image + noise1
+    image2 = clean_image + noise2
+
     correlations = fsc(image1, image2)
 
-    assert correlations.shape == image1.shape == image2.shape
+    assert sum(correlations[:10]) > sum(correlations[-40:-30])
