35fde2c902e2a8f6760bf5170ba48e0311497e9f
acs-ws <asolak@ku.edu.tr>
2022-09-08 13:31:01 -0700

    nicer n2sdataset

--- aydin/nn/models/utils/n2s_dataset.py
+++ aydin/nn/models/utils/n2s_dataset.py
@@ -4,19 +4,35 @@ from aydin.nn.util.random_sample_patches import random_sample_patches
 
 
 class N2SDataset(Dataset):
-    def __init__(self, image, patch_size):
-        """ """
+    def __init__(
+            self,
+            image,
+            patch_size,
+            nb_patches_per_image: int = 8,
+            adoption_rate: float = 0.5
+    ):
+        """
+
+        Parameters
+        ----------
+        image
+        patch_size
+        nb_patches_per_image
+        adoption_rate
+        """
         self.image = image
 
         self.crop_slicers = random_sample_patches(
             image,
-            patch_size=patch_size
+            patch_size=patch_size,
+            nb_patches_per_image=nb_patches_per_image,
+            adoption_rate=adoption_rate,
         )
 
     def __len__(self):
         return len(self.crop_slicers)
 
     def __getitem__(self, index):
-        input = self.image[self.crop_slicers[index]]
+        input_image = self.image[self.crop_slicers[index]]
 
-        return input
+        return input_image
