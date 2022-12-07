461e3ca9225d0da51f114fb45b22ba5d3348a35c
acs-ws <asolak@ku.edu.tr>
2022-12-07 09:16:35 -0800

    Black fix

--- aydin/it/test/test_cnn.py
+++ aydin/it/test/test_cnn.py
@@ -16,7 +16,8 @@ def test_it_cnn_jinet2D_light():
 
 def test_it_cnn_jinet3D_light():
     train_and_evaluate_cnn(
-        examples_single.myers_tribolium.get_array()[16:48, 300:332, 300:332], model="jinet"
+        examples_single.myers_tribolium.get_array()[16:48, 300:332, 300:332],
+        model="jinet",
     )
 
 
