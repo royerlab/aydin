00fd5848cb372b5ff3764059d88f22b8473fecb9
AhmetCanSolak <asolak14@ku.edu.tr>
2023-01-30 13:56:15 -0800

    analysis submodule is covered

--- aydin/analysis/test/test_empirical_noise_model.py
+++ aydin/analysis/test/test_empirical_noise_model.py
@@ -21,12 +21,3 @@ def test_noise_model():
     diff = numpy.abs(noisy_image - noisy_image_sampled)
 
     assert (diff < 60).all()
-
-    # import napari
-    #
-    # with napari.gui_qt():
-    #     viewer = napari.Viewer()
-    #     viewer.add_image(clean_image, name='clean_image')
-    #     viewer.add_image(noisy_image, name='noisy_image')
-    #     viewer.add_image(noisy_image_sampled, name='noisy_image_sampled')
-    #     viewer.add_image(diff, name='diff')
