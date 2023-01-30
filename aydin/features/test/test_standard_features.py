f847a55f9978affae2762d93664ba48cf47c73ea
AhmetCanSolak <asolak14@ku.edu.tr>
2023-01-30 13:22:15 -0800

    standard features test is parametrized

--- aydin/features/test/test_standard_features.py
+++ aydin/features/test/test_standard_features.py
@@ -1,21 +1,20 @@
 import numpy
+import pytest
 
 from aydin.features.standard_features import StandardFeatureGenerator
 from aydin.io.datasets import camera
 
 
-def test_standard_features_type_conservation():
-
-    _run_type_conservation_test(numpy.float16)
-    _run_type_conservation_test(numpy.float32)
-    _run_type_conservation_test(numpy.float64)
-    _run_type_conservation_test(numpy.uint8)
-    _run_type_conservation_test(numpy.uint16)
-    _run_type_conservation_test(numpy.uint32)
-    _run_type_conservation_test(numpy.uint64)
-
-
-def _run_type_conservation_test(dtype, required_dtype=None):
+@pytest.mark.parametrize("dtype", [
+    numpy.float16,
+    numpy.float32,
+    numpy.float64,
+    numpy.uint8,
+    numpy.uint16,
+    numpy.uint32,
+    numpy.uint64
+])
+def test_standard_features_type_conservation_test(dtype, required_dtype=None):
     image = camera().astype(dtype)
     # feature generator requires images in 'standard' form: BCTZYX
     image = image[numpy.newaxis, numpy.newaxis, ...]
