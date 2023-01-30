483563ee66c5145f6a29e99e90534b5b6b89f188
AhmetCanSolak <asolak14@ku.edu.tr>
2023-01-30 11:08:53 -0800

    linear scaling unit tests added

--- aydin/nn/models/test/test_linear_scaling_unet.py
+++ aydin/nn/models/test/test_linear_scaling_unet.py
@@ -4,25 +4,18 @@ import torch
 from aydin.nn.models.linear_scaling_unet import LinearScalingUNetModel
 
 
-@pytest.mark.parametrize("nb_unet_levels", [2])  # , 3, 5, 8])
-def test_forward_2D(nb_unet_levels):
-    input_array = torch.zeros((1, 1, 1024, 1024))
-    model2d = LinearScalingUNetModel(
+@pytest.mark.parametrize("input_array_shape, spacetime_ndim, nb_unet_levels", [
+    ((1, 1, 1024, 1024), 2, 2),
+    ((1, 1, 64, 64, 64), 3, 2),
+    ((1, 1, 64, 64, 64), 3, 3),
+    ((1, 1, 64, 64, 64), 3, 5),
+])
+def test_forward(input_array_shape, spacetime_ndim, nb_unet_levels):
+    input_array = torch.zeros(input_array_shape)
+    model = LinearScalingUNetModel(
         nb_unet_levels=nb_unet_levels,
-        spacetime_ndim=2,
+        spacetime_ndim=spacetime_ndim,
     )
-    result = model2d(input_array)
-    assert result.shape == input_array.shape
-    assert result.dtype == input_array.dtype
-
-
-@pytest.mark.parametrize("nb_unet_levels", [2, 3, 5])
-def test_forward_3D(nb_unet_levels):
-    input_array = torch.zeros((1, 1, 64, 64, 64))
-    model3d = LinearScalingUNetModel(
-        nb_unet_levels=nb_unet_levels,
-        spacetime_ndim=3,
-    )
-    result = model3d(input_array)
+    result = model(input_array)
     assert result.shape == input_array.shape
     assert result.dtype == input_array.dtype
