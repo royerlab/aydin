32261c3411a2965fd987c8e8d78d7f89f6f435c3
acs-ws <asolak@ku.edu.tr>
2023-04-10 11:51:35 -0700

    cleaner setup.py

--- setup.py
+++ setup.py
@@ -12,14 +12,6 @@ if sys.version_info < (3, 9):
     )
     sys.exit(1)
 
-# with open(os.path.join('requirements', 'default.txt')) as f:
-#     default_requirements = [
-#         line.strip() for line in f if line and not line.startswith('#')
-#     ]
-#
-# INSTALL_REQUIRES = default_requirements
-# REQUIRES = []
-
 setup(
     use_scm_version={"write_to": "aydin/_version.py"},
     setup_requires=['setuptools_scm'],
